91ef1bce938986d55a8a29d2b9159384b170825e
Lilly Vernon <littletomoya@yahoo.com>
2020-11-17 15:36:27 -0500

    Modified to accept GET request

--- world.php
+++ world.php
@@ -1,13 +1,24 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
-
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%".$_GET['country']."%'");
+$country=filter_input(INPUT_GET, 'country', FILTER_SANITIZE_NUMBER_INT);
+$stmt->bindParam('country',$country, PDO::PARAM_INT);
+$stmt-> execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+/* $country=$_GET;
+echo $_REQUEST['country']. "\n";
+print_r ($_GET['country'] . "\n");
+
+
+
+
 
 ?>
 <ul>
