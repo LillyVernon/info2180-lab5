8b337da5a50fd4add8034177a6010dcaefe3e046
Lilly Vernon <littletomoya@yahoo.com>
2020-11-17 16:28:29 -0500

    User clicks Lookup Button to get results

--- world.js
+++ world.js
@@ -6,19 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
         function(e) {
             country = document.getElementById("country").value;
             e.preventDefault()
-            if (data.value.length == 0) {
-                alert("hi")
-            } else {
-                var xmlhttp = new XMLHttpRequest();
-                xmlhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        alert(this.responseText);
-                    }
-                };
-                xmlhttp.open("GET", "http://localhost/info2180-lab5/world.php?country=" + country, true);
+                /*  if (data.value.length == 0) {
+                     alert("hi")
+                 } else { */
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("result").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "http://localhost/info2180-lab5/world.php?country=" + country, true);
 
-                xmlhttp.send();
-            }
+            xmlhttp.send();
+
 
 
         });
