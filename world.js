cc8538fe8fe5fba5cdf46df36e2b3f5ca6d83e95
Lilly Vernon <littletomoya@yahoo.com>
2020-11-19 20:47:50 -0500

    second button and query added and also the page was styled

--- world.js
+++ world.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     data = document.getElementById("country");
     btn = document.getElementById("lookup");
+    btn2 = document.getElementById("cities");
     btn.addEventListener('click',
         function(e) {
             country = document.getElementById("country").value;
@@ -18,10 +19,21 @@ document.addEventListener("DOMContentLoaded", function() {
             xmlhttp.open("GET", "http://localhost/info2180-lab5/world.php?country=" + country, true);
 
             xmlhttp.send();
-
-
-
         });
+    btn2.addEventListener('click',
+        function(e) {
+            country = document.getElementById("country").value;
+            e.preventDefault()
+            var xmlhttp2 = new XMLHttpRequest();
+            xmlhttp2.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("result").innerHTML = this.responseText;
+                
+                }
+            };
+            xmlhttp2.open("GET", "http://localhost/info2180-lab5/world.php?country=" + country + "&context=cities", true);
 
+            xmlhttp2.send();
+        });
 
 });
--- world.php
+++ world.php
@@ -6,35 +6,57 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%".$_GET['country']."%'");
+//$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%".$_GET['country']."%'");
+/* $country2=$_GET['country'];
+echo $country2;
+$stmt = $conn->query("SELECT c.district, c.name, c.population as city, c.country_code as country, c.population FROM cities c join countries cs on c.country_code = cs.code WHERE cs.name='".$_GET['country']."'");
 $country=filter_input(INPUT_GET, 'country', FILTER_SANITIZE_NUMBER_INT);
 $stmt->bindParam('country',$country, PDO::PARAM_INT);
-$stmt-> execute();
+$stmt-> execute(); 
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC); */
+
+
+
+
+$cities=$_GET['country'];
+
+ if (count($_GET)==2){
+$stmt = $conn->query("SELECT c.district, c.name, c.population as city, c.country_code as country, c.population FROM cities c join countries cs on c.country_code = cs.code WHERE cs.name='".$cities."'");
+$country=filter_input(INPUT_GET, 'country', FILTER_SANITIZE_NUMBER_INT);
+$stmt->bindParam('country',$country, PDO::PARAM_INT);
+$stmt-> execute(); 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-/* $country=$_GET;
-echo $_REQUEST['country']. "\n";
-print_r ($_GET['country'] . "\n");
-*/
-
-
-
-
- ?>
-<!-- <ul>
-<?php foreach ($results as $row): ?>
-<li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>  -->
-
-<table style="border-collapse: collapse; width: 100%; border: 1px solid black;">
+?>
+ <table style="border-collapse: collapse; width: 100%; border: 1px solid black;">
+<tr >
+    <th style="border: 1px solid black;">Name</th>
+    <th style="border: 1px solid black;">District</th>
+    <th style="border: 1px solid black;">Population</th>
+</tr>
+  <?php foreach ($results as $row): ?>
+    <tr:nth-child(even) style="background-color: #f2f2f2" >
+      <td style="border: 1px solid black;"><?= $row['name']; ?></td>
+      <td style="border: 1px solid black;"><?= $row['district']; ?></td>
+      <td style="border: 1px solid black;"><?= $row['population']; ?></td>
+      </tr>
+  <?php endforeach; ?>
+</table>
+<?php
+ }else{
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%".$_GET['country']."%'");
+  $country=filter_input(INPUT_GET, 'country', FILTER_SANITIZE_NUMBER_INT);
+  $stmt->bindParam('country',$country, PDO::PARAM_INT);
+  $stmt-> execute(); 
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+ <table style="border-collapse: collapse; width: 100%; border: 1px solid black;">
 <tr >
     <th style="border: 1px solid black;">Name</th>
     <th style="border: 1px solid black;">Continent</th>
     <th style="border: 1px solid black;">Independence</th>
     <th style="border: 1px solid black;">Head of State</th>
   </tr>
-  <?php foreach ($results as $row): ?>
+  <?php foreach ($result as $row): ?>
     <tr:nth-child(even) style="background-color: #f2f2f2" >
       <td style="border: 1px solid black;"><?= $row['name']; ?></td>
       <td style="border: 1px solid black;"><?= $row['continent']; ?></td>
@@ -43,3 +65,17 @@ print_r ($_GET['country'] . "\n");
       </tr>
   <?php endforeach; ?>
 </table>
+
+<?php } ?>
+ 
+ 
+
+
+ 
+<!-- <ul>
+<?php foreach ($results as $row): ?>
+<li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>  -->
+
+
